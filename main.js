b2f070346bf1d593fce1d0b1e4775692a603dd35
ShakibRamez <shakibramiz1@icloud.com>
2025-06-11 10:37:23 +0430

    object litreals

---
+++ main.js
@@ -0,0 +1,14 @@
+//object litreals
+let user = {
+    name: "shakib",
+    age: 20,
+    email: "shakibramiz3@gmail.com",
+    location: "herat",
+    blog: "web developer and student at computer since",
+};
+console.log(user);
+console.log(user.age);
+console.log(user['name']);
+
+user['name'] = "shakib jan";
+console.log(user['name']);
